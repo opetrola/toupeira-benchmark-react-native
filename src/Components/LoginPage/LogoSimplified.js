7b2e8656b460f5b274ebf234fc2990ecdfee6cc1
P37R0L4 <lucao.gadget@gmail.com>
2020-02-16 23:29:19 -0300

    Logo Redonda

--- src/Components/LoginPage/LogoSimplified.js
+++ src/Components/LoginPage/LogoSimplified.js
@@ -39,7 +39,7 @@ const styles = StyleSheet.create({
         borderColor: "#fff",
         borderWidth: 15,
         marginHorizontal: 5,
-        borderRadius: 8
+        borderRadius: 30
     },
 
     textLogo: {
